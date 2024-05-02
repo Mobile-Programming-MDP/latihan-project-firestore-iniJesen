5a2d31250f593d8250b94e30257e72148ad1eece
iniJesen <120288897+iniJesen@users.noreply.github.com>
2024-05-02 18:19:25 +0700

    tugas_1/5/24

---
+++ flutter_fire/lib/firebase_service.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_fire/userdata.dart';
+
+class FirebaseService {
+  static final COLLECTION_REF = 'user';
+
+  final firestore= FirebaseFirestore.instance;
+  late final CollectionReference userRef;
+
+  FirebaseService(){
+    userRef = firestore.collection(COLLECTION_REF);
+  }
+
+  Stream<QuerySnapshot<Object?>> ambilData(){
+    return userRef.snapshots();
+  }
+
+ void tambah(UserData userData){
+    DocumentReference documentReference = userRef.doc(userData.nama);
+    documentReference.set(userData.toJson());
+  }
+  
+  void hapus(UserData userData) {
+    DocumentReference documentReference = userRef.doc(userData.nama);
+    documentReference.delete();
+  }
+
+  void ubah(UserData userData) {
+    DocumentReference documentReference = userRef.doc(userData.nama);
+    documentReference.update(userData.toJson());
+  }
+
+}
