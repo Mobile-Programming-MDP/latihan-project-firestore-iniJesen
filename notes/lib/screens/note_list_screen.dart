71e34ce80f9daa215d5ac1c24eeb08274b7784b3
jesen <120288897+iniJesen@users.noreply.github.com>
2024-06-19 20:30:46 +0700

    last commit
    end

--- notes/lib/screens/note_list_screen.dart
+++ notes/lib/screens/note_list_screen.dart
@@ -1,9 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:notes/models/note.dart';
+import 'package:notes/screens/google_maps_screen.dart';
 import 'package:notes/services/note_service.dart';
 import 'package:notes/widgets/note_dialog.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+
 
 class NoteListScreen extends StatefulWidget {
-  const NoteListScreen({super.key});
+  final VoidCallback toggleTheme;
+
+  const NoteListScreen({Key? key, required this.toggleTheme}) : super(key: key);
 
   @override
   State<NoteListScreen> createState() => _NoteListScreenState();
@@ -15,6 +26,12 @@ class _NoteListScreenState extends State<NoteListScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Notes'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.dark_mode),
+            onPressed: widget.toggleTheme,
+          ),
+        ],
       ),
       body: const NoteList(),
       floatingActionButton: FloatingActionButton(
@@ -32,6 +49,7 @@ class _NoteListScreenState extends State<NoteListScreen> {
     );
   }
 }
+
 
 class NoteList extends StatelessWidget {
   const NoteList({super.key});
@@ -54,31 +72,141 @@ class NoteList extends StatelessWidget {
               padding: const EdgeInsets.only(bottom: 80),
               children: snapshot.data!.map((document) {
                 return Card(
-                  child: ListTile(
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return NoteDialog(note: document);
+                  child: Column(
+                    children: [
+                      document.imageUrl != null &&
+                              Uri.parse(document.imageUrl!).isAbsolute
+                          ? ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(16),
+                                topRight: Radius.circular(16),
+                              ),
+                              child: Image.network(
+                                document.imageUrl!,
+                                width: double.infinity,
+                                height: 150,
+                                fit: BoxFit.cover,
+                                alignment: Alignment.center,
+                              ),
+                            )
+                          : Container(),
+                      ListTile(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return NoteDialog(note: document);
+                            },
+                          );
                         },
-                      );
-                    },
-                    title: Text(document.title),
-                    subtitle: Text(document.description),
-                    trailing: InkWell(
-                      onTap: () {
-                        NoteService.deleteNote(document);
-                      },
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        child: Icon(Icons.delete),
+                        title: Text(document.title),
+                        subtitle: Text(document.description),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            InkWell(
+                              onTap: () async {
+                                Navigator.push(
+                                  context, 
+                                  MaterialPageRoute(
+                                    builder: (context) => GoogleMapsScreen(
+                                      latitude: double.parse(document.lat!), 
+                                      longitude: double.parse(document.lng!)
+                                    ),
+                                  ),
+                                );
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                child: Icon(Icons.location_on_outlined),
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            InkWell(
+                              onTap: () {
+                                _shareNote(document);
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                child: Icon(Icons.ios_share_outlined),
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            InkWell(
+                              onTap: () {
+                                showAlertDialog(context, document);
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                child: Icon(Icons.delete),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
                 );
               }).toList(),
             );
         }
+      },
+    );
+  }
+
+  void _shareNote(Note document) async {
+    try {
+      final response = await http.get(Uri.parse(document.imageUrl!));
+      final documentDirectory = (await getTemporaryDirectory()).path;
+      final imgFile = File('$documentDirectory/flutter.png');
+      imgFile.writeAsBytesSync(response.bodyBytes);
+
+      final message =
+          'Title: ${document.title}'
+          '\nDescription: ${document.description}'
+          '\nLocation: https://www.google.com/maps/search/?api=1&query=${document.lat},${document.lng}';
+
+      Share.shareXFiles([XFile(imgFile.path)], text: message);
+    } catch (e) {
+      print('Error sharing image: $e');
+    }
+  }
+
+  Future<void> _launchUrl(_url) async {
+    if(!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
+  }
+
+  showAlertDialog(BuildContext context, Note document) {
+    Widget cancelButton = ElevatedButton(
+      child: const Text("No"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+    Widget continueButton = ElevatedButton(
+      child: const Text("Yes"),
+      onPressed: () {
+        NoteService.deleteNote(document).whenComplete(() {
+          Navigator.of(context).pop();
+        });
+      },
+    );
+
+    AlertDialog alert = AlertDialog(
+      title: const Text("Delete Note"),
+      content: const Text("Are you sure to delete Note?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
       },
     );
   }
--- notes/lib/widgets/note_dialog.dart
+++ notes/lib/widgets/note_dialog.dart
@@ -1,43 +1,123 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:notes/models/note.dart';
+import 'package:notes/services/location_service.dart';
 import 'package:notes/services/note_service.dart';
 
-class NoteDialog extends StatelessWidget {
+class NoteDialog extends StatefulWidget {
   final Note? note;
+
+  NoteDialog({super.key, this.note});
+
+  @override
+  State<NoteDialog> createState() => _NoteDialogState();
+}
+
+class _NoteDialogState extends State<NoteDialog> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+  XFile? _imageFile;
+  Position? _position;
 
-  NoteDialog({super.key, this.note}) {
-    if (note != null) {
-      _titleController.text = note!.title;
-      _descriptionController.text = note!.description;
+  @override
+  void initState() {
+    super.initState();
+    if (widget.note != null) {
+      _titleController.text = widget.note!.title;
+      _descriptionController.text = widget.note!.description;
     }
+  }
+
+  Future<void> _getLocation() async {
+    final location = await LocationService().getCurrentLocation();
+    setState(() {
+      _position = location;
+    });
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+    final pickedFile = await ImagePicker().pickImage(source: source);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = pickedFile;
+      });
+    }
+  }
+
+  Future<void> _showImageSourceDialog() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Select Image Source'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _pickImage(ImageSource.camera);
+              },
+              child: const Text('Camera'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _pickImage(ImageSource.gallery);
+              },
+              child: const Text('Gallery'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text(note == null ? 'Add Notes' : 'Update Notes'),
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'Title: ',
-            textAlign: TextAlign.start,
-          ),
-          TextField(
-            controller: _titleController,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Text(
-              'Description: ',
+      title: Text(widget.note == null ? 'Add Notes' : 'Update Notes'),
+      content: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Title: ', textAlign: TextAlign.start),
+            TextField(controller: _titleController),
+            const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Text('Description: '),
             ),
-          ),
-          TextField(
-            controller: _descriptionController,
-          ),
-        ],
+            TextField(controller: _descriptionController),
+            const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Text('Image: '),
+            ),
+            SizedBox(
+              height: 200,
+              child: _imageFile != null
+                  ? Image.file(File(_imageFile!.path), fit: BoxFit.cover)
+                  : (widget.note?.imageUrl != null &&
+                          Uri.parse(widget.note!.imageUrl!).isAbsolute
+                      ? Image.network(widget.note!.imageUrl!, fit: BoxFit.cover)
+                      : Container()),
+            ),
+            TextButton(
+              onPressed: _showImageSourceDialog,
+              child: const Text('Pick Image'),
+            ),
+            TextButton(
+              onPressed: _getLocation,
+              child: const Text('Get Location'),
+            ),
+            Text(
+              _position?.latitude != null && _position?.longitude != null
+                  ? "Current Location : ${_position!.latitude.toString()}, ${_position!.longitude.toString()}"
+                  : "Current Location : ${widget.note?.lat}, ${widget.note?.lng}",
+              textAlign: TextAlign.start,
+            )
+          ],
+        ),
       ),
       actions: [
         Padding(
@@ -50,24 +130,41 @@ class NoteDialog extends StatelessWidget {
           ),
         ),
         ElevatedButton(
-          onPressed: () {
-            if (note == null) {
-              NoteService.addNote(Note(
-                title: _titleController.text,
-                description: _descriptionController.text,
-              )).whenComplete(() {
-                Navigator.of(context).pop();
-              });
+          onPressed: () async {
+            String? imageUrl = widget.note?.imageUrl;
+            if (_imageFile != null) {
+              imageUrl = await NoteService.uploadImage(_imageFile!);
+              if (imageUrl == null) {
+                // Show error message if the image upload failed
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Failed to upload image')),
+                );
+                return;
+              }
+            }
+
+            Note note = Note(
+              id: widget.note?.id,
+              title: _titleController.text,
+              description: _descriptionController.text,
+              imageUrl: imageUrl,
+              lat: widget.note?.lat.toString() != _position?.latitude.toString()
+                  ? _position?.latitude.toString()
+                  : widget.note?.lat.toString(),
+              lng: widget.note?.lng.toString() != _position?.longitude.toString()
+                  ? _position?.longitude.toString()
+                  : widget.note?.lng.toString(),
+              createdAt: widget.note?.createdAt,
+            );
+            if (widget.note == null) {
+              await NoteService.addNote(note);
+              Navigator.of(context).pop();
             } else {
-              NoteService.updateNote(Note(
-                id: note!.id,
-                title: _titleController.text,
-                description: _descriptionController.text,
-                createdAt: note!.createdAt,
-              )).whenComplete(() => Navigator.of(context).pop());
+              await NoteService.updateNote(note);
+              Navigator.of(context).pop();
             }
           },
-          child: Text(note == null ? 'Add' : 'Update'),
+          child: Text(widget.note == null ? 'Add' : 'Update'),
         ),
       ],
     );
